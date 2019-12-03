f6fd6b9496e4ad82364ee72f9c4b0733dbb3a5cb
Yodgy <42121861+Yodgy@users.noreply.github.com>
2019-12-03 20:37:30 +0300

    Added TextLookAt script

---
+++ Assets/Scripts/TextLookAt.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLookAt : MonoBehaviour
+{
+    [SerializedField]
+    private GameObject Text;
+}
