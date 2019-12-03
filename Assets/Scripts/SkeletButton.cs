3aa0876eb7424896a9f61df06d8f9a44222430ac
AlekseiBespalov <bespaloww2014@gmail.com>
2019-12-03 22:21:28 +0300

    Added TextLookAt script Added SkeletButton script(Needed fixes)

---
+++ Assets/Scripts/SkeletButton.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkeletButton : MonoBehaviour
+{
+    [SerializeField]
+    private float DeltaSize;
+
+    [SerializeField]
+    private GameObject TextObject;
+    
+    [SerializeField]
+    private float SmoothRotationValue;
+
+    [SerializeField]
+    private float SmoothScalingValue;
+
+    bool IsSwitched = false;
+    bool IsSized = false;
+
+    private void Start() 
+    {
+        TextObject.SetActive(false);
+    }
+
+    private void Update() 
+    {
+        if(IsSwitched)
+        {
+            RotateTowards();
+            ChangeSize();
+        }
+    }
+
+    private void RotateTowards()
+    {
+        float singleStep = SmoothRotationValue * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.parent.forward, -Camera.main.transform.forward, singleStep, 0.0f);
+        transform.parent.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    private void ChangeSize()
+    {
+        float singleStep = SmoothScalingValue * Time.deltaTime;
+        transform.localScale += new Vector3(DeltaSize, DeltaSize, DeltaSize) * singleStep;
+    }
+
+    private void OnMouseDown() 
+    {   
+        if(!IsSwitched)
+        {
+            transform.localScale += new Vector3(DeltaSize, DeltaSize, DeltaSize);
+            TextObject.SetActive(true);
+            IsSwitched = true;
+            return;
+        }
+        
+        if(IsSwitched)
+        {
+            transform.localScale -= new Vector3(DeltaSize, DeltaSize, DeltaSize);
+            TextObject.SetActive(false);
+            IsSwitched = false;
+            return;
+        }
+
+        // comment for Android touch input
+        // Touch touch;
+        // touch = Input.GetTouch(0);
+
+        // int fingerId = touch.fingerId;
+
+        // if(Input.touchCount == 1 && !EventSystem.current.IsPointerOverGameObject(fingerId) && !IsSwitched)
+        // {
+        //     transform.localScale = new Vector3(BaseLocalScale.x + DeltaSize, BaseLocalScale.y + DeltaSize, BaseLocalScale.z + DeltaSize);
+        //     TextObject.SetActive(true);
+        //     IsSwitched = true;
+        //     return;
+        // }
+        
+        // if(Input.touchCount == 1 && !EventSystem.current.IsPointerOverGameObject(fingerId) && IsSwitched)
+        // {
+        //     transform.localScale = new Vector3(BaseLocalScale.x - DeltaSize, BaseLocalScale.y - DeltaSize, BaseLocalScale.z - DeltaSize);
+        //     TextObject.SetActive(false);
+        //     IsSwitched = false;
+        //     return;
+        // }
+    }
+}
--- Assets/Scripts/TextLookAt.cs
+++ Assets/Scripts/TextLookAt.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class TextLookAt : MonoBehaviour
 {
-    [SerializedField]
-    private GameObject Text;
+    [SerializeField]
+    private GameObject TextForLookAt;
+
+    [SerializeField]
+    private float DistanceToEnableText;
+    
+    private float CurrentDistanceToText;
+
+    private void Update() 
+    {
+        TextForLookAt.transform.LookAt(Camera.main.transform);
+        TextForLookAt.transform.Rotate(Vector3.up - new Vector3(0, 180, 0));
+        DistanceToTextUpdate();
+        CheckDistanceToText();
+    }
+
+    private void DistanceToTextUpdate()
+    {
+        CurrentDistanceToText = Vector3.Distance(Camera.main.transform.position, transform.position);
+    }
+
+    private void CheckDistanceToText()
+    {
+        if(CurrentDistanceToText <= DistanceToEnableText)
+            TextForLookAt.SetActive(true);
+        else
+            TextForLookAt.SetActive(false);
+    }
 }
