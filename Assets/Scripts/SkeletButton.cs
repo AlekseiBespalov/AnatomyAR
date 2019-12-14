ecd584aed4c1adf822138f7fbdb1eab7709ae398
Yodgy <42121861+Yodgy@users.noreply.github.com>
2019-12-14 22:17:32 +0300

    comm

--- Assets/Scripts/SkeletButton.cs
+++ Assets/Scripts/SkeletButton.cs
@@ -6,64 +6,95 @@ using UnityEngine.EventSystems;
 public class SkeletButton : MonoBehaviour
 {
     [SerializeField]
+    private GameObject RootObject;
+
+    [SerializeField]
     private float DeltaSize;
+    [SerializeField]
+    private float MinSize;
+    private float MaxSize;
 
     [SerializeField]
     private GameObject TextObject;
-    
     [SerializeField]
     private float SmoothRotationValue;
 
-    [SerializeField]
-    private float SmoothScalingValue;
+    bool IsButtonPressed = false;
 
-    bool IsSwitched = false;
-    bool IsSized = false;
+    bool ObjectIsScaledUp;
+    bool ObjectIsScaledDown;
+
+    float currentX;
+    float currentY;
+    float currentZ;
+
 
     private void Start() 
     {
         TextObject.SetActive(false);
     }
 
-    private void Update() 
+    private void FixedUpdate() 
     {
-        if(IsSwitched)
-        {
-            RotateTowards();
-            ChangeSize();
-        }
+        currentX = transform.localScale.x;
+        currentY = transform.localScale.y;
+        currentZ = transform.localScale.z;
+
+        ChangeScale();
     }
 
     private void RotateTowards()
     {
         float singleStep = SmoothRotationValue * Time.deltaTime;
-        Vector3 newDirection = Vector3.RotateTowards(transform.parent.forward, -Camera.main.transform.forward, singleStep, 0.0f);
-        transform.parent.rotation = Quaternion.LookRotation(newDirection);
+        Vector3 newDirection = Vector3.RotateTowards(RootObject.transform.forward, -Camera.main.transform.forward, singleStep, 0.0f);
+        RootObject.transform.rotation = Quaternion.LookRotation(newDirection);
     }
 
-    private void ChangeSize()
+    public void ChangeScale()
     {
-        float singleStep = SmoothScalingValue * Time.deltaTime;
-        transform.localScale += new Vector3(DeltaSize, DeltaSize, DeltaSize) * singleStep;
+        if(currentX < MinSize && currentY < MinSize && currentZ < MinSize && IsButtonPressed)
+        {
+            transform.localScale = new Vector3(MinSize, MinSize, MinSize);
+            ObjectIsScaledDown = true;
+            ObjectIsScaledUp = false;
+            IsButtonPressed = false;
+            return;
+        }
+
+        if (currentX > MaxSize && currentY > MaxSize && currentZ > MaxSize && IsButtonPressed)
+        {
+            transform.localScale = new Vector3(MaxSize, MaxSize, MaxSize);
+            ObjectIsScaledDown = false;
+            ObjectIsScaledUp = true;
+            IsButtonPressed = false;
+            return;
+        }
+
+        if(IsButtonPressed && !ObjectIsScaledUp)
+        {
+            ScaleUp();
+            RotateTowards();
+            ObjectIsScaledDown = false;
+        }
+        if(!IsButtonPressed && !ObjectIsScaledDown)
+        {
+            ScaleDown();
+            ObjectIsScaledUp = false;
+        }
     }
+
+    public void ScaleUp()
+        {
+            transform.localScale = new Vector3(currentX + DeltaSize, currentY + DeltaSize, currentZ + DeltaSize);
+        }
+    public void ScaleDown()
+        {
+            transform.localScale = new Vector3(currentX - DeltaSize, currentY - DeltaSize, currentZ - DeltaSize);
+        }
 
     private void OnMouseDown() 
     {   
-        if(!IsSwitched)
-        {
-            transform.localScale += new Vector3(DeltaSize, DeltaSize, DeltaSize);
-            TextObject.SetActive(true);
-            IsSwitched = true;
-            return;
-        }
-        
-        if(IsSwitched)
-        {
-            transform.localScale -= new Vector3(DeltaSize, DeltaSize, DeltaSize);
-            TextObject.SetActive(false);
-            IsSwitched = false;
-            return;
-        }
+        IsButtonPressed = !IsButtonPressed;
 
         // comment for Android touch input
         // Touch touch;
