bef658d870efa145759231621a85b9cb199a978d
Yodgy <42121861+Yodgy@users.noreply.github.com>
2019-12-25 16:48:09 +0300

    New Script
    New Script

---
+++ Assets/Scripts/HideText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideText : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject TextObject;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    HidText();
+    }
+
+
+   
+
+
+    void HidText()
+     {
+         TextObject.SetActive(false);
+     }
+
+  
+
+
+}
