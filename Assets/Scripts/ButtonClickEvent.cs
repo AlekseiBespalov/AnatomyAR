581ccc6c2e29879966d62c490fa46d50d307eb94
AlekseiBespalov <bespaloww2014@gmail.com>
2019-12-25 17:02:35 +0300

    Added ButtonClickEvent script

---
+++ Assets/Scripts/ButtonClickEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonClickEvent : MonoBehaviour
+{
+    [SerializeField] UnityEvent Event;
+    [SerializeField] GameObject TextToHide;
+    
+    private void Start() 
+    {
+        TextToHide.SetActive(false);    
+    }
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+
+    public void Click()
+    {
+        TextToHide.SetActive(!TextToHide.active);
+    }
+}
