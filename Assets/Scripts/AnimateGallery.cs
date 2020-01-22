4b05d1adc07c5176a51f3c4c5ce628f9ca35c1e2
AlekseiBespalov <bespaloww2014@gmail.com>
2020-01-22 18:09:21 +0300

    Added side menu, main menu Added animations and animation player for button

---
+++ Assets/Scripts/AnimateGallery.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class AnimateGallery : MonoBehaviour
+{
+    public Animator menuAnimator;
+    [Header("SETTINGS")]
+    public bool openAtStart = false;
+    bool isOpen;
+    void Start()
+    {
+        if(openAtStart == true)
+        {
+            menuAnimator.Play("GalleryOpen");
+            isOpen = true;
+        }
+        //else
+        //{
+        //    menuAnimator.Play("Minimize");
+        //    animatedButton.Play("HTE Hamburger");
+        //    isOpen = false;
+        //}
+    }
+    public void Animate(bool state)
+    {
+        if (state == true)
+            menuAnimator.Play("GalleryClose");
+        else if(state == false)      
+            menuAnimator.Play("GalleryOpen");
+    }
+}
