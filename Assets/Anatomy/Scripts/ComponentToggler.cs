2d551b5d99f85bf89d602c5f6a01b473f629e69b
Yodgy <42121861+Yodgy@users.noreply.github.com>
2020-03-24 23:19:50 +0300

    add icon,text

---
+++ Assets/Anatomy/Scripts/ComponentToggler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentToggler : MonoBehaviour
+{
+    public void ToggleComponent(string componentName)
+    {
+    }
+}
