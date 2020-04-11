4986d1e0b32640c497c91f3603cf4753852d5985
AlekseiBespalov <bespaloww2014@gmail.com>
2020-04-11 17:11:59 +0300

    Toggle UI

---
+++ Assets/Anatomy/Scripts/MeatToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatToggle : MonoBehaviour
+{
+    public void TurnOffAllMeat()
+    {
+        List<GameObject> meats = new List<GameObject>();
+        meats.AddRange(GameObject.FindGameObjectsWithTag("Meat"));
+        List<MeshRenderer> meatMesh = new List<MeshRenderer>();
+        foreach(GameObject gameObject in meats)
+        {
+            meatMesh.Add(gameObject.GetComponent<MeshRenderer>());
+        }
+
+        foreach(MeshRenderer mesh in meatMesh)
+        {
+            mesh.enabled = false;
+        }
+    }
+
+    public void TurnOnAllMeat()
+    {
+        List<GameObject> meats = new List<GameObject>();
+        meats.AddRange(GameObject.FindGameObjectsWithTag("Meat"));
+        List<MeshRenderer> meatMesh = new List<MeshRenderer>();
+        foreach(GameObject gameObject in meats)
+        {
+            meatMesh.Add(gameObject.GetComponent<MeshRenderer>());
+        }
+
+        foreach(MeshRenderer mesh in meatMesh)
+        {
+            mesh.enabled = true;
+        }
+    }
+}
