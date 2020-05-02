dcf6a75b6ce921ef43f62f45983ee23d8fe160ec
Yodgy <42121861+Yodgy@users.noreply.github.com>
2020-05-02 21:53:30 +0300

    Fixed toggle meat script

--- Assets/Anatomy/Scripts/MeatToggle.cs
+++ Assets/Anatomy/Scripts/MeatToggle.cs
@@ -8,15 +8,23 @@ public class MeatToggle : MonoBehaviour
     {
         List<GameObject> meats = new List<GameObject>();
         meats.AddRange(GameObject.FindGameObjectsWithTag("Meat"));
-        List<MeshRenderer> meatMesh = new List<MeshRenderer>();
+        List<MeshRenderer> meatMeshes = new List<MeshRenderer>();
+        List<MeshCollider> meatColliders = new List<MeshCollider>();
+
         foreach(GameObject gameObject in meats)
         {
-            meatMesh.AddRange(gameObject.GetComponentsInChildren<MeshRenderer>());
+            meatColliders.AddRange(gameObject.GetComponentsInChildren<MeshCollider>());
+            meatMeshes.AddRange(gameObject.GetComponentsInChildren<MeshRenderer>());
         }
 
-        foreach(MeshRenderer mesh in meatMesh)
+        foreach(MeshRenderer mesh in meatMeshes)
         {
             mesh.enabled = false;
+        }
+
+        foreach(MeshCollider meatColider in meatColliders)
+        {
+            meatColider.enabled = false;
         }
     }
 
@@ -25,14 +33,22 @@ public class MeatToggle : MonoBehaviour
         List<GameObject> meats = new List<GameObject>();
         meats.AddRange(GameObject.FindGameObjectsWithTag("Meat"));
         List<MeshRenderer> meatMesh = new List<MeshRenderer>();
+        List<MeshCollider> meatColliders = new List<MeshCollider>();
+
         foreach(GameObject gameObject in meats)
         {
+            meatColliders.AddRange(gameObject.GetComponentsInChildren<MeshCollider>());
             meatMesh.AddRange(gameObject.GetComponentsInChildren<MeshRenderer>());
         }
 
         foreach(MeshRenderer mesh in meatMesh)
         {
             mesh.enabled = true;
+        }
+
+        foreach(MeshCollider meatColider in meatColliders)
+        {
+            meatColider.enabled = true;
         }
     }
 }
